9c7d41056efd3178dda2cbfae9dd85a497ee8c89
Obsidian <ObsidianRock@users.noreply.github.com>
2016-12-14 11:16:45 +0000

    adding profile view to profile blueprint

--- app/profile/views.py
+++ app/profile/views.py
@@ -10,12 +10,12 @@ def profile():
     pagination = Volunteer.query.paginate(page,
                                           per_page=current_app.config['VOL_PER_PAGE'],
                                           error_out=False)
-    vol = pagination.items
+    volunteer_object = pagination.items
     if pagination.pages < 2:
         index_page = None
     else:
         index_page = True
     return render_template('profile/profiles.html',
-                           vol=vol,
+                           volunteers=volunteer_object,
                            pagination=pagination,
                            index=index_page)
